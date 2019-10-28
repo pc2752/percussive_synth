fecd4eb04d03e1440cbc407cf8787d71c526b188
pc2752 <pc2752@gmail.com>
2019-10-28 16:13:00 +0100

    Resuing scope

--- models.py
+++ models.py
@@ -835,6 +835,7 @@ class PercSynth(Model):
         """
 
         with tf.variable_scope('NPSS') as scope:
+            scope.reuse_variables()
             self.output = modules.full_network(self.cond_placeholder,self.input_placeholder,  self.is_train)
 
 class PercSynthEncode(Model):
