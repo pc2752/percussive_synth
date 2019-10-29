0f7dffc501e9006ff902e81774d6d31becc80f86
xavierfav <xavier.favory@gmail.com>
2019-10-29 18:47:59 +0100

    Add default parameter value

--- notebook_utils.py
+++ notebook_utils.py
@@ -56,6 +56,11 @@ model_widgets_params = {
 CURRENT_MODEL_NAME = DEFAULT_MODEL
 
 
+# not used, just here for keeping good example values
+DEFAULT_KICKS_VALUES = [0.46533436, 0.6132435, 0.6906892, 0.5227648, 0.6955591, 0.733622, 0.4321724]
+DEFAULT_FS_VALUES = [0.7279065, 0.7787189, 0.4476448, 0.7108625, 0.39033762, 0.4943667, 0.5660683]
+
+
 def generate_envelope(attack, release, sr=16000):
     envelope = []
     if attack + release <= 1 :
@@ -69,13 +74,13 @@ def return_widgets(callback):
     model_name = widgets.Dropdown(**model_widgets_params)
     attack = widgets.FloatSlider(**attack_widgets_params)
     release = widgets.FloatSlider(**release_widgets_params)
-    brightness = widgets.FloatSlider(description='Brightness', value=random(), **default_widgets_params)
-    hardness = widgets.FloatSlider(description='Hardness', value=random(), **default_widgets_params)
-    depth = widgets.FloatSlider(description='Depth', value=random(), **default_widgets_params)
-    roughness = widgets.FloatSlider(description='Roughness', value=random(), **default_widgets_params)
-    boominess = widgets.FloatSlider(description='Boominess', value=random(), **default_widgets_params)
-    warmth = widgets.FloatSlider(description='Warmth', value=random(), **default_widgets_params)
-    sharpness = widgets.FloatSlider(description='Sharpness', value=random(), **default_widgets_params)
+    brightness = widgets.FloatSlider(description='Brightness', value=0.46, **default_widgets_params)
+    hardness = widgets.FloatSlider(description='Hardness', value=0.61, **default_widgets_params)
+    depth = widgets.FloatSlider(description='Depth', value=0.69, **default_widgets_params)
+    roughness = widgets.FloatSlider(description='Roughness', value=0.52, **default_widgets_params)
+    boominess = widgets.FloatSlider(description='Boominess', value=0.69, **default_widgets_params)
+    warmth = widgets.FloatSlider(description='Warmth', value=0.73, **default_widgets_params)
+    sharpness = widgets.FloatSlider(description='Sharpness', value=0.43, **default_widgets_params)
 
     out = widgets.interactive_output(callback, {
         'model_name': model_name,
